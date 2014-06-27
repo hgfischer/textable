63d7026ee43cbb555fa3b3de28a7f7be49ed4f47
Herbert G. Fischer <herbert.fischer@gmail.com>
2014-06-26 22:14:26 -0300

    Delete deprecated test and add more types of data

--- textable_test.go
+++ textable_test.go
@@ -14,10 +14,10 @@ var (
 		{"Australia", uint(23536500), 4.5, 0.938, 3},
 		{"Brazil", uint(202756000), 5.1, 0.730, 4},
 		{"Canada", uint(35427524), 5.5, 0.911, 2},
-		{"China", uint(1365220000), 4.3, 0.699, 10},
+		{"China", uint(1365220000), nil, 0.699, 10},
 		{"Greece", uint(10816286), 4.0, 0.860, -6},
 		{"India", uint(1245830000), 3.1, 0.554, 7},
-		{"Kazakhstan", uint(17244000), 2.8, 0.754, 10},
+		{"Kazakhstan", uint(17244000), 2.8, "?", 10},
 		{"Portugal", uint(10477800), 5.2, 0.816, -1},
 		{"Russia", uint(146000000), 3.9, 0.788, 6},
 		{"United States", uint(318275000), 5.5, 0.937, 3},
@@ -44,20 +44,5 @@ func TestAddRowShouldSetProperTypeForEachColumnAndAppendValues(t *testing.T) {
 func TestAddRowWithWrongNumberOfColumnsShouldReturnError(t *testing.T) {
 	tt := New(TEST_HEADERS...)
 	err := tt.AddRow()
-	assert.NotNil(t, err)
-}
-
-func TestAddRowWithWrongTypeOfColumnShouldReturnError(t *testing.T) {
-	tt := New(TEST_HEADERS...)
-	err := tt.AddRow(TEST_DATA[0]...)
-	assert.Nil(t, err)
-	wrongData := []interface{}{
-		1,   // StringCol accepts everything
-		1.2, // UIntCol also accepts floats, dropping the decimal part
-		10,  // FloatCol also accepts integers
-		1,   // Same as above
-		"-", // This should fail, since this string does not have a number inside
-	}
-	err = tt.AddRow(wrongData...)
 	assert.NotNil(t, err)
 }
